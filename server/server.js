cc861b34f48ff761dda6c1a57dd9d0575c34874f
brodie-m <brodiedmcguire@gmail.com>
2021-09-09 11:47:28 +0100

    updated backend

--- server/server.js
+++ server/server.js
@@ -1,45 +1,46 @@
 const express = require('express')
 const cors = require('cors')
 const getUsers = require('./models/users')
-const {
-    tsParameterProperty
-} = require('@babel/types')
+const userRouter = require('./controllers/userRouter')
+
 
 const app = express()
 app.use(cors())
+app.use('/',userRouter)
+app.use('/', userRouter)
 const port = 3000;
 app.listen(port, () => {
     console.log(`Express running on ${port}`)
 })
 
-app.get('/users', async (req, res) => {
-    const users = await getUsers();
+// app.get('/users', async (req, res) => {
+//     const users = await getUsers();
 
-    res.send(users.data)
-})
+//     res.send(users.data)
+// })
 
-app.get('/users/:name', async (req, res) => {
-    const users = await getUsers();
-    // const foundUser = users.data.findIndex(x => x.id === req.params.id)
-    // res.send(foundUser)
-    try {
-        const userData = await users.data;
+// app.get('/users/:name', async (req, res) => {
+//     const users = await getUsers();
+//     // const foundUser = users.data.findIndex(x => x.id === req.params.id)
+//     // res.send(foundUser)
+//     try {
+//         const userData = await users.data;
 
-        const foundUser = userData.filter((user) => {
-            return user.firstName == req.params.name
-        })
+//         const foundUser = userData.filter((user) => {
+//             return user.firstName == req.params.name
+//         })
 
-        if (foundUser.length === 0) {
-            console.log('error')
-            return res.status(404).send("No user found")
-        } else {
-            res.send(foundUser)
-        }
-    } catch (error) {
-        res.status(500).send('server error')
-    }
+//         if (foundUser.length === 0) {
+//             console.log('error')
+//             return res.status(404).send("No user found")
+//         } else {
+//             res.send(foundUser)
+//         }
+//     } catch (error) {
+//         res.status(500).send('server error')
+//     }
 
-})
+// })
 
 // const CognitiveServicesCredentials = require('@azure/ms-rest-azure-js').CognitiveServicesCredentials;
 // const WebSearchAPIClient = require('@azure/cognitiveservices-websearch');
