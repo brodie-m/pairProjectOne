145a49c98867ab4fc92bc019c15194ba914a2372
brodie-m <brodiedmcguire@gmail.com>
2021-09-09 11:59:08 +0100

    more changes

--- client/script.js
+++ client/script.js
@@ -1,34 +1,38 @@
 const searchButton = document.getElementById('google-search-button')
-searchButton.addEventListener('click', async (e) => {
+
+async function dealsWithClick(e) {
     e.preventDefault();
     console.log('clicked')
     const query = getSearchQuery();
     if (!query) {
         return
     }
-    
+    //page conversion
     document.querySelector('.search-page').classList.add('hidden');
     document.getElementById('results-card').classList.remove('hidden');
     document.getElementById('nav-middle').classList.toggle('hidden');
     
     const resultsBar = document.getElementById('resultsBar')
-    resultsBar.setAttribute("placeholder",`${query}`)
-    //     fetch('http://localhost:3000/')
-    //         .then(response => response.json().forEach((e) => {
-    //             console.log(e)
-    //             document.getElementById('card-text').innerHTML += e
-    //         }))
+    resultsBar.setAttribute("placeholder", `${query}`)
+    //end page conversion
 
+
+    //make card
+    
     const users = await fetch(`http://localhost:3000/users/${query}`)
     const allUser = await users.json()
     console.log(allUser)
-    for (let i =0; i < allUser.length ; i++) {
+    generateCard(allUser)
+}
+
+function generateCard(allUser) {
+    for (let i = 0; i < allUser.length; i++) {
         //create card
         const card = document.createElement('div');
         card.classList.add('card');
         //create image and change attributes, add class
         const img = document.createElement('img')
-        img.setAttribute("src",`${allUser[i].picture}`)
+        img.setAttribute("src", `${allUser[i].picture}`)
         img.classList.add('card-image-round')
         //create card title
         const title = document.createElement('h1')
@@ -40,24 +44,32 @@ searchButton.addEventListener('click', async (e) => {
         card.append(title);
         card.append(someLorem)
         document.querySelector('.card-host').append(card)
-
-        // //append
-        // document.getElementById('card-title').innerHTML = `${allUser[i].firstName}  ${allUser[i].lastName}`
-        // document.getElementById('card-image').setAttribute("src",`${allUser[i].picture}`)
     }
-    
+}
+
+
+searchButton.addEventListener('click', e => dealsWithClick(e))
+
+
+// //append
+// document.getElementById('card-title').innerHTML = `${allUser[i].firstName}  ${allUser[i].lastName}`
+// document.getElementById('card-image').setAttribute("src",`${allUser[i].picture}`)
 
 
 
-})
+
+
+
 
 function getSearchQuery() {
     const searchQuery = document.getElementById('searchBarText').value;
     return searchQuery;
 }
 
+
+
 function generateInterests(users) {
-    const interests = ['coding','cooking','baking','badminton','football','gym','chess','test driven development']
+    const interests = ['coding', 'cooking', 'baking', 'badminton', 'football', 'gym', 'chess', 'test driven development']
     for (let i = 0; i < users.length; i++) {
         users[i].interests = ``
     }
