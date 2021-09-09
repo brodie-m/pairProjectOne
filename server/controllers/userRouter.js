2b8cbd9febed9a122c0cad26d0f29770915e1d1a
brodie-m <brodiedmcguire@gmail.com>
2021-09-09 20:14:09 +0100

    added search by image tags

--- server/controllers/userRouter.js
+++ server/controllers/userRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const usersRouter = express.Router();
 const getUsers = require('../models/users');
+const getPosts = require('../models/posts');
 
 
 
@@ -12,18 +13,31 @@ usersRouter.get('/users', async (req, res) => {
 
 usersRouter.get('/users/:name', async (req, res) => {
     const users = await getUsers();
+    const posts = await getPosts();
     // const foundUser = users.data.findIndex(x => x.id === req.params.id)
     // res.send(foundUser)
     try {
         const userData = await users.data;
-
-        const foundUser = userData.filter((user) => {
+        const postsData= await posts.data;
+        userData.splice(0,(userData.length-postsData.length))
+        //add posts data to users data
+        for (let i = 0; i < userData.length; i++) {
+            userData[i].post = postsData[i];
+        }
+        console.log(userData)
+        //search by name
+        let foundUser = userData.filter((user) => {
             return user.firstName == req.params.name
         })
-
+        //search by tags
         if (foundUser.length === 0) {
-            console.log('error')
-            return res.status(404).send("No user found")
+            let foundUser = userData.filter((user) => {
+                return user.post.tags.includes(req.params.name)
+                })
+            res.send(foundUser);
+            // need to re-implement error handling
+            // console.log('error')
+            // return res.status(404).send("No user found")
         } else {
             res.send(foundUser)
         }
